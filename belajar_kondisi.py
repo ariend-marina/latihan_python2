0bd4f27276059dfba76600c7d386209936bf74eb
ariend-marina <marinaazizah98@guru.smk.belajar.id>
2025-07-11 13:49:09 +0700

    belajar_kondisi.py

---
+++ belajar_kondisi.py
@@ -0,0 +1,16 @@
+# Contoh penggunaan kondisi dalam Python
+
+# Meminta input dari pengguna
+nilai = int(input("Masukkan nilai Anda: "))
+
+# Mengevaluasi kondisi
+if nilai >= 90:
+    print("Nilai Anda: A")
+elif nilai >= 80:
+    print("Nilai Anda: B")
+elif nilai >= 70:
+    print("Nilai Anda: C")
+elif nilai >= 60:
+    print("Nilai Anda: D")
+else:
+    print("Nilai Anda: E (Tidak Lulus)")
